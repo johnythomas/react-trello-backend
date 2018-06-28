7c7ad0c919dfcbef5fe5417789281afbc36c0fb6
Johny Thomas <johnythomas007@gmail.com>
2018-06-28 23:21:37 +0530

    Feat: Add 404 error handler

--- src/index.js
+++ src/index.js
@@ -11,5 +11,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use("/board", board)
 
+app.use((req, res, next) => {
+  const err = new Error("Not Found")
+  err.status = 404
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message
+  res.locals.error = req.app.get("env") === "development" ? err : {}
+  res.status(err.status || 500).send(err)
+})
+
 const { PORT = 8080 } = process.env
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`)) // eslint-disable-line no-console
